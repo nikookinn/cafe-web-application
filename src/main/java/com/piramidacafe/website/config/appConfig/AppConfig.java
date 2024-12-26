469c6dbf627f7358e5bb49397b2da66d29e0a4d0
skywalker <nikookinn@hotmail.com>
2024-12-26 19:13:48 +0400

    properties file has been adapted to railway mysql database variable

--- src/main/java/com/piramidacafe/website/config/appConfig/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.piramidacafe.website.config.appConfig;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:my-private.properties")
-public class AppConfig {
-}
