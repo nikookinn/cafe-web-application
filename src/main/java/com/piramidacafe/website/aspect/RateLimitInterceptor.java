1c2fc423d1ca32d64a9ceb38576eff3ddb6dec01
skywalker <nikookinn@hotmail.com>
2025-04-10 00:32:59 +0400

    log added rate limit interceptor class

--- src/main/java/com/piramidacafe/website/aspect/RateLimitInterceptor.java
+++ src/main/java/com/piramidacafe/website/aspect/RateLimitInterceptor.java
@@ -5,6 +5,7 @@ import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -15,13 +16,18 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
+@Slf4j
 public class RateLimitInterceptor implements HandlerInterceptor {
     private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         String ip = getClientIp(request);
+        String path = request.getRequestURI();
         Bucket bucket = buckets.computeIfAbsent(ip, this::newBucket);
+
+        log.info("Request from IP: {}, Path: {}, User-Agent: {}",
+                ip, path, request.getHeader("User-Agent"));
 
         if (bucket.tryConsume(1)) {
             response.addHeader("X-Rate-Limit-Remaining", String.valueOf(bucket.getAvailableTokens()));
