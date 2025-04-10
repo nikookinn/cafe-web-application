46c6bd383938eb02b31fb56ec387221f2e914bba
skywalker <nikookinn@hotmail.com>
2025-04-10 17:33:40 +0400

    website icon removed

--- src/main/java/com/piramidacafe/website/controller/GlobalControllerAdvice.java
+++ src/main/java/com/piramidacafe/website/controller/GlobalControllerAdvice.java
@@ -3,23 +3,17 @@ package com.piramidacafe.website.controller;
 import com.piramidacafe.website.dto.ContactInformationDto;
 import com.piramidacafe.website.service.ContactInformationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalControllerAdvice {
-    @Value("${app.image.upload.dir}")
-    private String imageUploadDir;
 
     private final ContactInformationService contactInformationService;
 
     @ModelAttribute("info")
-    public ContactInformationDto getContactInformation(Model model){
-        String websiteIcon = imageUploadDir + "/app_icon/1736280265356_170084cd-be38-4082-a9ce-e271436e797d.ico";
-        model.addAttribute("websiteIcon", websiteIcon);
+    public ContactInformationDto getContactInformation(){
         return contactInformationService.findContactInfo();
     }
 }
