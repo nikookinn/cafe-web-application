a9aa6e24fa82008839d9aa5aa43c7500eb7e44cf
skywalker <nikookinn@hotmail.com>
2025-06-29 01:10:14 +0400

    logger added to file downloader controller

--- src/main/java/com/piramidacafe/website/controller/FileDownloadController.java
+++ src/main/java/com/piramidacafe/website/controller/FileDownloadController.java
@@ -1,6 +1,8 @@
 package com.piramidacafe.website.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +12,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
+@Slf4j
 @RestController
 @RequestMapping("/admin/api")
 public class FileDownloadController {
     @Value("${app.image.upload.dir}")
     private String uploadDir;
+
+
 
     @GetMapping("/downloadAll")
     public void downloadAllFiles(HttpServletResponse response) throws IOException {
@@ -33,6 +38,7 @@ public class FileDownloadController {
                 ZipOutputStream zos = new ZipOutputStream(response.getOutputStream());
                 Stream<Path> paths = Files.walk(folderPath)
         ) {
+            log.info("file download start... :"+ folderPath);
             paths
                     .filter(Files::isRegularFile)
                     .forEach(filePath -> {
@@ -42,11 +48,11 @@ public class FileDownloadController {
                             Files.copy(filePath, zos);
                             zos.closeEntry();
                         } catch (IOException e) {
-                            // loglama yapabilirsin
-                            e.printStackTrace();
+                        log.error("something went wrong while downloading files from volume");
                         }
                     });
         }
+        log.info("files successfully downloaded from corresponding docker volume :"+ folderPath);
     }
 
 }
